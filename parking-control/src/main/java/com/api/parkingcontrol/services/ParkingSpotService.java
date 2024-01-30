666f24c910d978852179122b4fd9e812a5311ce1
brunolemes318 <brunoleme318@gmail.com>
2024-01-30 01:34:21 -0300

    feat: added models, repository, service

---
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,18 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.repository.ParkingSpotRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+    @Autowired
+     ParkingSpotRepository parkingSpotRepository;
+
+    public ParkingSpotService(ParkingSpotRepository parkingSpotRepository){
+        this.parkingSpotRepository = parkingSpotRepository;
+
+    }
+
+
+}
